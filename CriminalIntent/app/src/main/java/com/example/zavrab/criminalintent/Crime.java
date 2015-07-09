3c43789a65f2c72d458b45a17aa3bffde129ed9a
Zaver Rabadiya <zaver.rabadiya@gmail.com>
2015-07-08 18:06:53 -0700

    Chapter 8 done

--- CriminalIntent/app/src/main/java/com/example/zavrab/criminalintent/Crime.java
+++ CriminalIntent/app/src/main/java/com/example/zavrab/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.example.zavrab.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -9,6 +10,14 @@ public class Crime {
 
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        //Generate unique identifier
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
 
     public UUID getId() {
         return mId;
@@ -22,8 +31,20 @@ public class Crime {
         mTitle = title;
     }
 
-    public Crime() {
-        //Generate unique identifier
-        mId = UUID.randomUUID();
+    public Date getDate() {
+        return mDate;
     }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
 }
--- CriminalIntent/app/src/main/java/com/example/zavrab/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/example/zavrab/criminalintent/CrimeFragment.java
@@ -1,12 +1,18 @@
 package com.example.zavrab.criminalintent;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 /**
@@ -16,11 +22,16 @@ public class CrimeFragment extends Fragment {
 
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
+
+    private static final String TAG = "CrimeFragment";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mCrime = new Crime();
+        Log.d(TAG, "onCreate(Bundle) called");
     }
 
     @Override
@@ -44,6 +55,51 @@ public class CrimeFragment extends Fragment {
 
             }
         });
+
+        String crimeDate = new DateFormat().format("EEEE, MMM dd, yyyy", mCrime.getDate()).toString();
+
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        mDateButton.setText(crimeDate);
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //Set the crime's solved property
+                mCrime.setSolved(isChecked);
+            }
+        });
         return v;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 }
