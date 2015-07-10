c5ac96c8e3d1fb93a004f135788a06a515d34543
Zaver Rabadiya <zaver.rabadiya@gmail.com>
2015-07-10 15:00:57 -0700

    Chapter 11 done

--- CriminalIntent/app/src/main/java/com/example/zavrab/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/example/zavrab/criminalintent/CrimeFragment.java
@@ -42,8 +42,12 @@ public class CrimeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        UUID crimeId = (UUID) getArguments().getSerializable(ARG_CRIME_ID);
-        mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
+        if(getArguments() != null) {
+            UUID crimeId = (UUID) getArguments().getSerializable(ARG_CRIME_ID);
+            mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
+        } else {
+            mCrime = new Crime();
+        }
     }
 
     @Override
