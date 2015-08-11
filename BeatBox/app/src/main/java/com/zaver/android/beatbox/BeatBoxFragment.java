f7296053007ddfdda139afe3bd8dd36aefc13d97
Zaver Rabadiya <zaver.rabadiya@gmail.com>
2015-08-10 18:21:09 -0700

    Chapter 19 Done

--- BeatBox/app/src/main/java/com/zaver/android/beatbox/BeatBoxFragment.java
+++ BeatBox/app/src/main/java/com/zaver/android/beatbox/BeatBoxFragment.java
@@ -25,6 +25,7 @@ public class BeatBoxFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRetainInstance(true);
 
         mBeatBox = new BeatBox(getActivity());
     }
@@ -40,8 +41,14 @@ public class BeatBoxFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mBeatBox.release();
+    }
+
     // ==================================== Private classes ========================================
-    private class SoundHolder extends RecyclerView.ViewHolder {
+    private class SoundHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         private Button mButton;
         private Sound mSound;
@@ -49,11 +56,17 @@ public class BeatBoxFragment extends Fragment {
             super(inflater.inflate(R.layout.list_item_sound, container, false));
 
             mButton = (Button) itemView.findViewById(R.id.list_item_sound_button);
+            mButton.setOnClickListener(this);
         }
 
         public void bindSound(Sound sound) {
             mSound = sound;
             mButton.setText(mSound.getName());
+        }
+
+        @Override
+        public void onClick(View v) {
+            mBeatBox.play(mSound);
         }
     }
 
